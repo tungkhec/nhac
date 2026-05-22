e43a7e53cf2017cd5a48cf1433c3c1d6cbe52c8d
tungkhec <tctctcty@hotmail.com>
2026-05-22 16:41:04 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -105,7 +105,7 @@ const musicData = [
     album: "Nhạc Sĩ...",
     year: 2025,
     artist: "Thùy Chi - Minh Phương",
-    musicPath: "https://drive.google.com/uc?export=download&id=/1TjSQpfmPUNVloNEZ3MWrIM5vOKfhnFGA",
+    musicPath: "https://drive.google.com/uc?export=view&id=1TjSQpfmPUNVloNEZ3MWrIM5vOKfhnFGA"
   },
   {
     backgroundImage: "./assets/images/a.jpg",
